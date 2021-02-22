290acfd932f68ff76d574bb7d1635b844dc24452
Den <test@gmail.com>
2021-02-22 06:34:57 +0300

    change

--- src/utils/getDataLocal.js
+++ src/utils/getDataLocal.js
@@ -5,6 +5,7 @@ const LOCAL_PLAYLIST = "https://raw.githubusercontent.com/JSDenis/Mus_proj/main/
 //const RADIO = 'https://cors-anywhere.herokuapp.com/https://api.deezer.com/radio&output=json';
 /* const JSON_RADIO = 'http://localhost:3006/radio';
 const JSON_PLAYLIST = 'http://localhost:3006/playlist'; */
+const LOCAL_DATA = 'https://raw.githubusercontent.com/JSDenis/Mus_proj/main/src/data/data.json';
 
 console.log(LOCAL_RADIO);
 
@@ -23,29 +24,39 @@ const getResource = async (url) => {
 }
 
 const getAllRadio = async () => {
-  debugger;
-  const res = await getResource(LOCAL_RADIO);
-  return res.data.map(_transformRadio).slice(0, 5);
+  const res = await getResource(LOCAL_DATA);
+  //return res.data.map(_transformRadio).slice(0, 5);
+  return res.radio.map(_transformRadio).slice(0, 5);
 }
 
 const getRadio = async (id) => {
-  debugger;
-  const res = await getResource(`${LOCAL_RADIO}`);
-  return res.data.map((item, index) => {
+  const res = await getResource(`${LOCAL_DATA}`);
+  // return res.data.map((item, index) => {
+  //   _transformConcreteRadio(item, id)
+  // });
+  return res.radio.map((item, index) => {
     _transformConcreteRadio(item, id)
   });
 }
 
 const getRadioCurrent = async (id) => {
-  debugger;
-  const res = await getResource(`${LOCAL_RADIO}`);
-  return res.data.map((item, index) => {
-    _transformConcreteRadio(item, id)
+  const res = await getResource(`${LOCAL_DATA}`);
+  console.log(res.radio.find((item, index) => {
+    if(item.id == id){
+      //console.log(item);
+      return item;
+    }
+  }));
+  return res.radio.find((item, index) => {
+    //_transformConcreteRadio(item, id)
+    if(item.id == id){
+      //console.log(item);
+      return item;
+    }
   }).tracklistLocal[0].preview;
 }
 
 const getAllPlaylist = async () => {
-  debugger;
   const res = await getResource(LOCAL_PLAYLIST);
   return res.tracks.data.map(_transformPlayList).slice(0, 5);
 }
